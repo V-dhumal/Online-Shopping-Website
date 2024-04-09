dde3503240922dce4fbc5b85892028d192469a2a
V-dhumal <dhumalvaishnavi9356@gmail.com>
2024-04-09 12:14:27 +0530

    commit

--- client/src/views/OrderNow/OrderNow.js
+++ client/src/views/OrderNow/OrderNow.js
@@ -7,6 +7,8 @@ function OrderNow() {
         <Navbar />
         <h1 style={{textAlign: 'center',margin: '150px',fontSize: '70px'}}> OrderNow 🏡 </h1>
         <p>Order Now Page</p>
+        <h2>Ordered Successfully!!!</h2>
+        <input type='text' placeholder='Enter Your Product' />
     </div>
   )
 }
